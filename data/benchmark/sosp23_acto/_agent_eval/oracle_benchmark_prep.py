ade5f9242b0aaab00ee3be97d9bfc2221f4441e1
Bogdan-Alexandru Stoica <bastoica@illinois.edu>
2026-05-02 00:33:28 -0500

    chore: clean up stale code and push upstream

--- data/benchmark/sosp23_acto/_agent_eval/oracle_benchmark_prep.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-class OracleBenchmarkPrep:
-
-  def __init__(self):
-    return # fall through
-
-  def run(self):
-    return True # fall through, no benchmarks installed by Acto
