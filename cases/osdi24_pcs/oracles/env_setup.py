944a28c523be3a0106ad063f3665b19411444186
Bogdan-Alexandru Stoica <bastoica@illinois.edu>
2026-05-29 15:24:31 -0500

    refactor: reimplement oracles to use class inheritence only

--- cases/osdi24_pcs/oracles/env_setup.py
+++ cases/osdi24_pcs/oracles/env_setup.py
@@ -5,31 +5,29 @@ from collections.abc import Sequence
 from evaluator.oracles import utils
 from evaluator.oracles.case_base import CaseOracleEnvSetupBase
 from evaluator.oracles.env_setup_checks import (
-	DependencyVersionCheck,
-	FilesystemPathCheck,
-	PathType,
+    DependencyVersionCheck,
+    FilesystemPathCheck,
+    PathType,
 )
 
 
 class OracleEnvSetup(CaseOracleEnvSetupBase):
-	def requirements(self) -> Sequence[utils.BaseCheck]:
-		repo_root = self.paths.workspace_dir
-
-		return (
-			DependencyVersionCheck(
-				name="python3",
-				cmd=("python3", "--version"),
-				min_version=(3, 10, 0),
-			),
-			DependencyVersionCheck(
-				name="pip",
-				cmd=("python3", "-m", "pip", "--version"),
-				min_version=(0, 0, 0),
-				optional=True,
-			),
-			FilesystemPathCheck(
-				name="repo_root_exists",
-				path=repo_root,
-				path_type=PathType.DIRECTORY,
-			),
-		)
+    def requirements(self) -> Sequence[utils.BaseCheck]:
+        return (
+            DependencyVersionCheck(
+                name="python3_version",
+                cmd=("python3", "--version"),
+                min_version=(3, 10, 0),
+            ),
+            DependencyVersionCheck(
+                name="pip_available",
+                cmd=("python3", "-m", "pip", "--version"),
+                min_version=(0, 0, 0),
+                optional=True,
+            ),
+            FilesystemPathCheck(
+                name="repo_root_exists",
+                path=self.paths.workspace_dir,
+                path_type=PathType.DIRECTORY,
+            ),
+        )
