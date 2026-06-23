4b583b81e8b44301c8fa91bb7a20e5242a9a9005
ArshiAAkhavan <letmemakenewone@gmail.com>
2026-06-22 18:53:34 -0700

    oracle 3/4

--- cases/asplos24_crocus/oracles/benchmark_prep.py
+++ cases/asplos24_crocus/oracles/benchmark_prep.py
@@ -2,18 +2,34 @@ from __future__ import annotations
 
 from collections.abc import Sequence
 
-from evaluator.oracles import CaseOracleBenchmarkPrepBase
-from evaluator.oracles.checks import PathKind
+from evaluator.oracles import CaseOracleBenchmarkPrepBase, PathKind
+from evaluator.oracles.utils import BaseCheck
 
-from .case_constants import EXPECTED_RESULT_REF
+from .consts import CASE_STUDY_ISLE_FILES, EXPERIMENT_SCRIPTS
 
 
 class OracleBenchmarkPrep(CaseOracleBenchmarkPrepBase):
-    def requirements(self) -> Sequence:
-        return (
-            self.path_check(
-                name="expected_reference_exists",
-                path=self.ref_path(EXPECTED_RESULT_REF),
-                kind=PathKind.FILE,
-            ),
-        )
+	"""Verify the experiment inputs (case-study ISLE files and scripts) are present."""
+
+	def requirements(self) -> Sequence[BaseCheck]:
+		checks: list[BaseCheck] = []
+
+		for rel in CASE_STUDY_ISLE_FILES:
+			checks.append(
+				self.path_check(
+					name=f"isle_{rel}",
+					path=self.app_path(rel),
+					kind=PathKind.FILE,
+				)
+			)
+
+		for rel in EXPERIMENT_SCRIPTS:
+			checks.append(
+				self.path_check(
+					name=f"script_{rel}",
+					path=self.app_path(rel),
+					kind=PathKind.FILE,
+				)
+			)
+
+		return tuple(checks)
