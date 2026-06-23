b6cde4ec8bb2d6b1827bcfdf919adaaf245038db
ArshiAAkhavan <letmemakenewone@gmail.com>
2026-06-22 17:45:54 -0700

    oracle 1/4

--- cases/asplos24_crocus/oracles/env_setup.py
+++ cases/asplos24_crocus/oracles/env_setup.py
@@ -2,18 +2,39 @@ from __future__ import annotations
 
 from collections.abc import Sequence
 
-from evaluator.oracles import CaseOracleEnvSetupBase
-from evaluator.oracles.checks import PathKind
+from evaluator.oracles import (
+	CaseOracleEnvSetupBase,
+	PathCheck,
+	PathKind,
+	VersionCheck,
+)
+from evaluator.oracles.utils import BaseCheck
 
-from .case_constants import INSTRUCTION_PATH
+from .consts import DOCKER_MIN_VERSION, DOCKERFILE_PATH, README_PATH
 
 
 class OracleEnvSetup(CaseOracleEnvSetupBase):
-    def requirements(self) -> Sequence:
-        return (
-            self.path_check(
-                name="instructions_exist",
-                path=self.workspace_path(INSTRUCTION_PATH),
-                kind=PathKind.FILE,
-            ),
-        )
+	def requirements(self) -> Sequence[BaseCheck]:
+
+		return (
+			VersionCheck(
+				name="docker",
+				cmd=("docker", "--version"),
+				min_version=DOCKER_MIN_VERSION,
+			),
+			PathCheck(
+				name="repo_root_exists",
+				path=self.artifact_path(),
+				kind=PathKind.DIRECTORY,
+			),
+			PathCheck(
+				name="dockerfile_exists",
+				path=self.artifact_path(DOCKERFILE_PATH),
+				kind=PathKind.FILE,
+			),
+			PathCheck(
+				name="readme_exists",
+				path=self.artifact_path(README_PATH),
+				kind=PathKind.FILE,
+			),
+		)
