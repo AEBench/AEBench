846bd1ce7264f1daca89937b24d07aaac7cde40d
Eric Zhang <eric23zhang@gmail.com>
2026-08-04 14:42:58 -0500

    Corrected case.toml to be corect runtime and added details, fixed artifact build

--- cases/asplos24_morphqpv/oracles/artifact_build.py
+++ cases/asplos24_morphqpv/oracles/artifact_build.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import os
 from collections.abc import Sequence
 
-from evaluator.oracles import utils
+from evaluator.oracles.reporting import BaseCheck, PathKind
 from evaluator.oracles.bases import CaseOracleArtifactBuildBase
 
 _DEFAULT_CONDA_ENV_NAME = "morphenv"
@@ -19,10 +19,22 @@ _REQUIRED_IMPORTS = (
 class OracleArtifactBuild(CaseOracleArtifactBuildBase):
     """Verify the morphenv Conda environment contains the correctly built dependencies and versions."""
 
-    def requirements(self) -> Sequence[utils.BaseCheck]:
+    def requirements(self) -> Sequence[BaseCheck]:
         env_name = os.environ.get("AE_MORPHQPV_CONDA_ENV", _DEFAULT_CONDA_ENV_NAME).strip()
 
         return (
+            self.path_check(
+                name="requirements_exists",
+                path=self.workspace_path("requirements.txt"),
+                kind=PathKind.FILE,
+            ),
+            self.path_check(
+                name="requirements_no_version_exists",
+                path=self.workspace_path("requirementswithnoversion.txt"),
+                kind=PathKind.FILE,
+            ),
+
+
             self.command_check(
                 name=f"conda_env_{env_name}_exists",
                 cmd=(
@@ -35,7 +47,7 @@ class OracleArtifactBuild(CaseOracleArtifactBuildBase):
             self.version_check(
                 name="qiskit_version",
                 cmd=("conda", "run", "-n", env_name, "python", "-c", "import qiskit; print(qiskit.__version__)"),
-                min_version=(0, 44, 1),
+                min_version=(0, 25, 1),
             ),
             # Check PennyLane Version
             self.version_check(
